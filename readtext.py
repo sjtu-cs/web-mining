3a926b007310295ea21dfb4034775573c1c1ce18
Martin Xu <martin.xus@gmail.com>
2017-06-10 01:48:54 +0800

    set encoding to utf-8

--- readtext.py
+++
@@ -1,6 +0,0 @@
- #! /usr/bin/env python
- positive_examples = list(open('/data/train.txt', "r").readlines())
- positive_examples = [s.strip() for s in positive_examples]
- labels = [s[0] for s in positive_examples]
- records = [s[2:] for s in positive_examples]
- print(records)
--- text_cnn.py
+++ text_cnn.py
@@ -1,3 +1,6 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import tensorflow as tf
 import numpy as np
 
