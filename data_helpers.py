5e80f9b7d533ed3db7c8e9fa70a085e9350aa3cb
tomatohill <hill_0623@qq.com>
2017-06-27 16:10:05 +0800

    参考，修改过
    参考，修改过

--- data_helpers.py
+++ data_helpers.py
@@ -1,12 +1,35 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import numpy as np
 import re
 import itertools
 from collections import Counter
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+def numbers_to_vector(argument):
+    list=[]
+    for each in argument:
+        switcher = {
+         "0": [1,0,0,0,0],
+         "1": [0,1,0,0,0],
+         "2": [0,0,1,0,0],
+         "3": [0,0,0,1,0],
+         "4": [0,0,0,0,1],
+               }
+        list.append(switcher.get(each, [0, 0, 0, 0, 0]))
+    return list
 
 
+stop = {'ourselves', 'hers', 'between', 'yourself', 'again', 'there', 'about', 'once', 'during',
+       'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its', 'yours', 'such', 'into', 'of',
+        'most', 'itself', 'other', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the',
+        'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don', 'nor', 'me',
+        'were', 'her', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 'above', 'both', 'to',
+         'ours', 'had', 'she', 'all', 'when', 'at', 'any', 'before', 'them', 'and', 'been', 'have', 'in', 'will',
+         'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'so', 'can', 'did', 'now', 'he', 'you',
+         'herself', 'has', 'just', 'where', 'only', 'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 't',
+        'being', 'if', 'theirs', 'my', 'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here',
+        'than', 'doe', 'character', 'rrb', 'lrb', 'story', 'one', 'ha', 'wa'}
 def clean_str(string):
     """
     Tokenization/string cleaning for all datasets except for SST.
@@ -25,54 +48,76 @@ def clean_str(string):
     string = re.sub(r"\)", " \) ", string)
     string = re.sub(r"\?", " \? ", string)
     string = re.sub(r"\s{2,}", " ", string)
-
+    #停用词
+   # string = ' '.join([word for word in string.split() if word not in stop])
+    #词形还原
+    #lemmatizer = WordNetLemmatizer()
+   # string = ''.join([lemmatizer.lemmatize(word) for word in string])
     return string.strip().lower()
+def load_test_data_and_labels(lable_data_file):
+    lable_examples = list(open(lable_data_file, "r", -1, "utf-8").readlines())
+    lable_examples = [s.strip() for s in lable_examples]
+    #temp2[0]))
+    x_text = [clean_str(sent[2:]) for sent in lable_examples]
+    lab = [sent[0] for sent in lable_examples]
 
+    return [x_text, numbers_to_vector(lab)]
 
-def load_data_and_labels(data_file, with_labels = True, raw_data = False):
+def load_data(lable_data_file):
+    lable_examples = list(open(lable_data_file, "r", -1, "utf-8").readlines())
+    lable_examples = [s.strip() for s in lable_examples]
+    x_text = lable_examples
+    x_text2 = [clean_str(sent) for sent in x_text]
+    x_text=[sent for sent in x_text]
+    return[x_text2,x_text]
+
+def load_data_and_labels(d0_data_file, d1_data_file,d2_data_file,d3_data_file,d4_data_file):
     """
     Loads MR polarity data from files, splits the data into words and generates labels.
     Returns split sentences and labels.
     """
     # Load data from files
+    degree0_examples = list(open(d0_data_file, "r",-1,"utf-8").readlines())
+    degree0_examples = [s.strip() for s in degree0_examples]
+    degree1_examples = list(open(d1_data_file, "r",-1,"utf-8").readlines())
+    degree1_examples = [s.strip() for s in degree1_examples]
+    degree2_examples = list(open(d2_data_file, "r", -1, "utf-8").readlines())
+    degree2_examples = [s.strip() for s in degree2_examples]
+    degree3_examples = list(open(d3_data_file, "r", -1, "utf-8").readlines())
+    degree3_examples = [s.strip() for s in degree3_examples]
+    degree4_examples = list(open(d4_data_file, "r", -1, "utf-8").readlines())
+    degree4_examples = [s.strip() for s in degree4_examples]
+    # Split by words
+    x_text = degree0_examples + degree1_examples+degree2_examples+degree3_examples+degree4_examples
+    x_text = [clean_str(sent) for sent in x_text]
+    # Generate labels
+    degree0_labels = [[1, 0, 0, 0, 0] for _ in degree0_examples]
+    degree1_labels = [[0, 1, 0, 0, 0] for _ in degree1_examples]
+    degree2_labels = [[0, 0, 1, 0, 0] for _ in degree2_examples]
+    degree3_labels = [[0, 0, 0, 1, 0] for _ in degree3_examples]
+    degree4_labels = [[0, 0, 0, 0, 1] for _ in degree4_examples]
+    y = np.concatenate([degree0_labels, degree1_labels,degree2_labels,degree3_labels,degree4_labels], 0)
+    return [x_text, y]
 
-    # {"Very Negative", "Negative", "Neutral", "Positive", "Very Positive"}
-    x_raw = []
-    with open(data_file, 'r') as f:
-        x_raw = [s.strip() for s in list(f.readlines())]
-
-    if raw_data:
-        return x_raw
-    else:
-        x_text = []
-
-        if with_labels:
-            labels = []
-
-            label_mappings = {
-                '0': [1,0,0,0,0],
-                '1': [0,1,0,0,0],
-                '2': [0,0,1,0,0],
-                '3': [0,0,0,1,0],
-                '4': [0,0,0,0,1]
-            }
-
-            # Default label for unkonw label
-            default_label = [0,0,1,0,0]
-
-            for sample in x_raw:
-                x_text.append(clean_str(sample[2:]))
-                labels.append(label_mappings.get(sample[0], default_label))
-
-            y_label = np.concatenate([labels], 0)
-
-        else:
-            for sample in x_raw:
-                x_text.append(clean_str(sample))
-
-            y_label = None
-
-        return [x_text, y_label]
+def load_data_and_labels2(data_file):
+    datalist = list(open(data_file, "r", -1, "utf-8").readlines())
+    datalist = [s.strip() for s in datalist]
+    labels = [s[0] for s in datalist]
+    records = [s[2:] for s in datalist]
+    x_text = [clean_str(sent) for sent in records]
+    lab_v = []
+    for index in range(len(labels)):
+        if labels[index] == '0':
+            lab_v.append([1, 0, 0, 0, 0])
+        elif labels[index] == '1':
+            lab_v.append([0, 1, 0, 0, 0])
+        elif labels[index] == '2':
+            lab_v.append([0, 0, 1, 0, 0])
+        elif labels[index] == '3':
+            lab_v.append([0, 0, 0, 1, 0])
+        elif labels[index] == '4':
+            lab_v.append([0, 0, 0, 0, 1])
+    return [x_text, lab_v]
 
 def batch_iter(data, batch_size, num_epochs, shuffle=True):
     """
